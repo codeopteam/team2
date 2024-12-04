aef1e02665777f1411a861c435324c49f5cd8f90
Yalina Gómez <yalinita1985@gmail.com>
2024-12-04 15:07:47 +0100

    keydown  Enter in input Search and link to Results page

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,13 +4,25 @@
        <div class="flex justify-center">
         <div>
         <h1 class="text-2xl font-bold text-white mt-8"> Don't miss out!</h1>
-        <h1 class="text-2xl font-bold text-white mt-2">Explore the <span class="text-yellow-400">vibrant events</span> happening in Spain</h1>
+        <h1 class="text-2xl font-bold text-white mt-2">Explore the <span class="text-yellow-400">vibrant events</span> happening in Spain.</h1>
       
         <input type="text" 
         class="mt-4 rounded-sm bg-[url('../../Lupa.png')] bg-no-repeat bg-[left_10px_center] bg-[length:20px_20px] pl-9 w-full pb-1" 
-        placeholder="City">
+        placeholder="City"
+        @keydown.enter="handleKeyDown">
       </div> 
     </div>     
       
     </div>
   </template>
+
+  <script>
+export default {
+  name: "Hero",
+  methods: {
+    handleKeyDown() {
+      this.$router.push("/results"); // Redirige a la ruta '/results'
+    }
+  }
+}
+</script>
