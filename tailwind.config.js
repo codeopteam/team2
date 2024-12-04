846cbb5b8eeb1d08cd8401b9bce3bed9a050e8ac
AlejandroMurr <elkinmoore@gmail.com>
2024-12-04 12:58:34 +0100

    creación de variables de estilos global

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: [ "./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+
+      darkBlue: "#2B293D",
+      yellowApp: "#FFE047",
+
+      },
+
+      fontFamily:{
+
+        montserrat:[ "Monserrat", "sans-serif"],
+
+      }
+
+
+    },
   },
   plugins: [],
 }
