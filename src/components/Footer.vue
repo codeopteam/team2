5935f2447fdf48b151518b7bd16089f4fe0bc323
AlejandroMurr <elkinmoore@gmail.com>
2024-12-05 13:23:17 +0100

    Complete footer layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,109 @@
 <template>
-    <footer class="bg-gray-800 text-white p-4 text-center">
-      <p>© 2023 Plataforma de Eventos. Todos los derechos reservados.</p>
+    <footer class="bg-darkBlue text-white py-10">
+
+      <div class="container mx-auto px-10 lg:px-20 grid grid-cols-5 md:gap-8">
+
+        <div class="flex flex-col">
+
+        <h3 class="text-white font-montserrat text-xl mb-4"> Company Info</h3>
+
+        <ul class="space-y-2 text-xs font-light">
+
+          <li> <a href="#" class="hover:text-white">About Us</a></li>
+          <li> <a href="#" class="hover:text-white">Contact Us</a> </li>
+          <li> <a href="#" class="hover:text-white">Careers</a> </li>
+          <li> <a href="#" class="hover:text-white">FAQs</a> </li>
+          <li> <a href="#" class="hover:text-white">Terms of Service</a> </li>
+          <li> <a href="#" class="hover:text-white">Privacy Policy</a> </li>
+
+        </ul>
+
+      </div>
+
+      <div class="flex flex-col">
+
+        <h3 class="text-white font-montserrat text-xl mb-4"> Help</h3>
+
+        <ul class="space-y-2 text-xs font-light">
+
+          <li> <a href="#" class="hover:text-white">Account Support</a></li>
+          <li> <a href="#" class="hover:text-white">Listing Events</a> </li>
+          <li> <a href="#" class="hover:text-white">event Ticketing</a> </li>
+          <li> <a href="#" class="hover:text-white">Ticket Purchase Terms & Conditions</a> </li>
+        
+
+        </ul>
+
+      </div>
+
+      <div class="flex flex-col">
+
+        <h3 class="text-white font-montserrat text-xl mb-4"> Categories</h3>
+
+        <ul class="space-y-2 text-xs font-light">
+
+          <li> <a href="#" class="hover:text-white">Concert & Gigs</a></li>
+          <li> <a href="#" class="hover:text-white">Festival & Lifestyle</a> </li>
+          <li> <a href="#" class="hover:text-white">Business & Networking</a> </li>
+          <li> <a href="#" class="hover:text-white">Food & Drinks</a> </li>
+          <li> <a href="#" class="hover:text-white">Performing Arts</a> </li>
+          <li> <a href="#" class="hover:text-white">Sports & Outdoors</a> </li>
+          <li> <a href="#" class="hover:text-white">Exhibitions</a> </li>
+          <li> <a href="#" class="hover:text-white">Workshops, Conferences & Classes</a> </li>
+
+
+        </ul>
+
+      </div>
+
+      <div class="flex flex-col">
+        <h3 class="text-white font-montserrat text-xl mb-4"> Follow Us</h3>
+
+        <ul class="space-y-2 text-xs font-light">
+
+        <li> <a href="#" class="hover:text-white">Facebook</a></li>
+          <li> <a href="#" class="hover:text-white">Instagram</a> </li>
+          <li> <a href="#" class="hover:text-white">Twitter</a> </li>
+          <li> <a href="#" class="hover:text-white"> Youtube</a> </li>
+
+        </ul>
+
+
+      </div>
+
+      <div class="flex flex-col">
+  <h3 class="text-white font-montserrat text-xl mb-4">Download the App</h3>
+
+  <div class="flex items-center border border-gray-500 rounded p-2 mb-4">
+    <img src="/public/logo google play.png" alt="Google Play" class="w-12 h-auto ml-2 mr-4">
+    <div>
+      <p class="text-white text-sm">Get it on</p>
+      <p class="text-white text-lg font-monserrat">Google Play</p>
+    </div>
+  </div>
+
+  <div class="flex items-center border border-gray-500 rounded p-2">
+    <img src="/public/logo appel.png" alt="App Store" class="w-6 h-auto ml-4 mr-8">
+    <div>
+      <p class="text-white text-sm">Download on the</p>
+      <p class="text-white text-lg font-monserrat">App Store</p>
+    </div>
+  </div>
+</div>
+
+</div>
+
+<div class="bg-darkBlue py-4 border-t border-gray-600 mt-8">
+
+  <div class="container mx-auto text-white text-xs">
+    <p class="mb-4 md:mb-0 text-center"> © 2024 EvenTop. All rights reserved.</p>
+
+  </div>
+
+
+
+         </div>
     </footer>
 
-    <div class="bg-darkBlue text-yellowApp font-montserrat p-4">Prueba de color</div>
   </template>
   
