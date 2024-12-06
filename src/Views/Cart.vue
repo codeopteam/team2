c857ab8c937a28d3bdb81d55cffbbc28247e1ed2
AlejandroMurr <elkinmoore@gmail.com>
2024-12-06 14:02:53 +0100

    add title event in cart

--- src/Views/Cart.vue
+++ src/Views/Cart.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
     <h1 class="text-2xl font-bold">Cart</h1>
+    <ul v-for="item in cartStore.items">
+      <li>{{ item.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+
+import { useCartStore } from '../stores/cartStore';
+import { mapStores } from 'pinia';
+
 export default {
     name: "Cart",
+    computed: {
+        ...mapStores(useCartStore)
+
+    },
 };
+
+
 </script>
