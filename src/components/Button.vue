b2d00426d828d00387bbb42691857ec36234ad24
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2024-12-09 13:12:37 +0100

    Icon ticket added

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="w-[115px] h-[40px] bg-yellowApp font-montserrat rounded-xl text-darkBlue text-m fotn-medium">Sing up</button>
+    <button class="w-[115px] h-[40px] bg-yellowApp font-montserrat rounded-xl text-darkBlue text-m font-medium">Sing up</button>
   </template>
   <!-- <script>
   export default {
