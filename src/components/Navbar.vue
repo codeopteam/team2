a75842c0ea88eb9a98637495e87e63e52d7fc230
mava <mavafiza@gmail.com>
2024-12-12 16:09:54 +0100

    more fixings in header responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,56 +1,49 @@
 <template>
-  <nav class="p-4 flex flex-wrap justify-between items-center space-y-4 md:space-y-0">
-    <ul class="flex flex-wrap space-x-4 md:space-x-8">
-      <li v-for="link in navLeft" :key="link.path">
-        <router-link :to="link.path"
-          class="text-white-600 hover:border-b-2 font-montserrat tracking-wide hover:border-yellowApp">
-          {{ link.sectionName }}
-        </router-link>
+<nav class="p-4 flex flex-wrap justify-between items-center space-y-4 md:space-y-0">
+    <ul class="flex space-x-8">
+      <li  v-for="link in navLeft" :key="link.path" >
+        <router-link :to="link.path" class="text-white-600 hover:border-b-2 font-montserrat tracking-wide hover:border-yellowApp">{{ link.sectionName }}</router-link>
       </li>
+     
     </ul>
-    <ul class="flex flex-wrap items-center space-x-4 md:space-x-8">
+     <ul class="flex space-x-8">
       <li v-for="link in navRight" :key="link.path">
-        <router-link :to="link.path"
-          class="text-white hover:border-b-2 font-montserrat tracking-wide hover:border-yellowApp">
-          {{ link.sectionName }}
-        </router-link>
+        <router-link :to="link.path" class="text-white hover:border-b-2 font-montserrat tracking-wide hover:border-yellowApp">{{  link.sectionName }}</router-link>
       </li>
+      
       <!-- <li v-if="cartStore.showTickets"> -->
-
       <li>
-        <router-link to="/cart" class="flex items-center font-montserrat text-yellowApp space-x-2">
-          <img src="/ion_ticket.png" alt="Ticket icon" class="h-6 w-6">
-          <span>{{ cartStore.cartSize }}</span>
-        </router-link>
+        <button>
+          <router-link to="/cart" class="flex font-montserrat text-yellowApp "> <img src="/ion_ticket.png" alt="Ticket icon" class="h-6 w-6 mr-2">{{ cartStore.cartSize }}</router-link>
+        </button>
       </li>
     </ul>
   </nav>
 </template>
 
 
-
 <script>
-import { useCartStore } from "../stores/cartStore"
+import  { useCartStore } from "../stores/cartStore"
 import { mapStores } from "pinia";
 
 export default {
   name: "NavBar",
-  props:
-    ["navLeft", "navRight"],
-  data() {
-    return {
+  props: 
+     ["navLeft", "navRight"],
+     data(){
+      return {
 
-    };
-  },
+      };
+     },
+     
+    computed: {
+      ...mapStores(useCartStore)
+    },
+    // mounted() {
+    //   this.cartStore.getItemsFromFirebase()
 
-  computed: {
-    ...mapStores(useCartStore)
-  },
-  // mounted() {
-  //   this.cartStore.getItemsFromFirebase()
-
-  // }
+    // }
 
 };
 
-</script>
+</script> 
