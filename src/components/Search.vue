416ac38f79f0c8e3e164f73787ba98de60a9030a
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2024-12-12 13:16:56 +0100

    wip search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,12 +3,13 @@
     <h2 class="font-montserrat font-semibold text-xl">Filters</h2>
     <input v-model="cityUser" type="text" label="City" placeholder="City"
       class="bg-slate-200 rounded px-2 placeholder:text-darkGray h-10 font-montserrat">
-    <input type="date" placeholder="Date" class="bg-slate-200 rounded px-2 h-10 text-darkGray font-montserrat">
-    <select class="bg-slate-200 rounded px-2 h-10 w-full text-darkGray font-montserrat">
+    <input v-model="date" type="date" placeholder="Date" class="bg-slate-200 rounded px-2 h-10 text-darkGray font-montserrat">
+    <select v-model="eventType" class="bg-slate-200 rounded px-2 h-10 w-full text-darkGray font-montserrat">
       <option value="0">Event type</option>
-      <option value="1">Sport</option>
+      <option v-for="type in eventTypes" :key="type.id" :value="type.name">{{  type.name }}</option>
+      <!-- <option value="1">Sport</option>
       <option value="2">Movie</option>
-      <option value="3">Music</option>
+      <option value="3">Music</option> -->
     </select>
     <button @click="emitSearch" class="bg-yellowApp rounded px-2 h-10 font-montserrat">Find</button>
   </div>
@@ -20,16 +21,21 @@ export default {
   data() {
     return {
       cityUser: "",
-      // date: "",
-      // eventType: "",
+       date: "",
+       eventType: "",
+       eventTypes: [
+        {id: 1, name: "Sport"},
+        {id: 1, name: "Movie"},
+        {id: 1, name: "Music"},
+       ]
     }
   },
   methods: {
     emitSearch() {
 
       if (this.cityUser.trim()) {
-        this.$emit('search', this.cityUser) // Emitimos la ciudad al componente padre
-        // console.log("City entered:", this.cityUser);
+        this.$emit('search', this.cityUser, this.date, this.eventType) // Emitimos la ciudad al componente padre
+         console.log("City entered:", this.cityUser, this.date, this.eventType);
 
       } else {
         alert("Por favor, ingresa una ciudad válida.");
