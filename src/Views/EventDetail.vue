f7497c5f73cc21c5fa18c1f1cfd226de5e22a20a
Yalina Gómez <yalinita1985@gmail.com>
2024-12-12 13:11:23 +0100

    fetch a eventDetail por id de evento

--- src/Views/EventDetail.vue
+++ src/Views/EventDetail.vue
@@ -1,18 +1,17 @@
 <template>
 
-    <div>
-      <h1 class="text-2xl font-bold">Event Name: {{ eventStore.item.name }}</h1>
-      <h4>startDateTime: {{ eventStore.item.sales.public.startDateTime }}</h4>
+    <div v-if="eventDetailresp">
+      <h1 class="text-2xl font-bold">Event Name: {{ eventDetailresp.name }}</h1>
+      <h4>startDateTime: {{ eventDetailresp.sales.public.startDateTime }}</h4>
       <!-- <h4>Date: {{ eventStore.item.dates[start][localDate] }}</h4> -->
       <!-- <h4>Hora: {{ eventStore.item.dates[start][localTime] }}</h4>  -->
-      <img :src="eventStore.item.images[2]?.url" alt="Event Image"/>
+      <img :src="eventDetailresp.images[2]?.url" alt="Event Image"/>
    
     </div>
   </template>
 
   <script>
-  // import { useEventStore } from '../stores/eventStore';
-  // import { mapStores } from 'pinia';
+  
   import axios from "axios";
 
   export default {
@@ -20,24 +19,25 @@
   
   data(){
    return {
+   eventDetailresp: null,
    loading: false,
    error: "",
    }
   },
   methods: {
-    performSearch(id) {     
+    performSearch() {   
+        
       this.loading = true;
       this.error = "";
 
       const apiKey = import.meta.env.VITE_API_KEY;
-
-      // const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&id=${id}`;
-      // let url = `https://app.ticketmaster.eu/mfxapi/v2/events/{event_id}`;
-      let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}/{event_id}`;
+     
+      let url = `https://app.ticketmaster.com/discovery/v2/events/${this.$route.params.id}.json?apikey=${apiKey}`;    
 
       axios(url)
-        .then((response) => {
-          let event = response.data._embedded.events/{event_id};                     
+        .then((response) => {          
+          console.log(response)  
+          this.eventDetailresp = response.data           
           
         })
         .catch((err) => {
@@ -49,14 +49,8 @@
         });
     },
   },
-  computed: {
-        ...mapStores(useEventStore)
-        
-    },
     mounted() {
-      console.log("Event Store:", this.eventStore.item); 
-      // hacer fetch con id del eventCard
-      this.performSearch(this.$route.params.id)
+      this.performSearch()     
     }   
 }
   </script>
