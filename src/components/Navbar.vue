ca9bc562395a3fe0f2e1405b258008cce1590c13
mava <mavafiza@gmail.com>
2024-12-12 18:43:27 +0100

    aligning button tickets

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,31 @@
 <template>
   <nav class="flex w-full justify-between">
-    <ul class="flex space-x-8">
+    <ul class="flex space-x-8 items-center">
       <li v-for="link in navLeft" :key="link.path">
-        <router-link 
-          :to="link.path" 
+        <router-link :to="link.path"
           class="text-white-600 hover:border-b-2 font-montserrat tracking-wide hover:border-yellowApp">
           {{ link.sectionName }}
         </router-link>
       </li>
     </ul>
 
-    <ul class="flex space-x-8 pr-8">
+    <ul class="flex space-x-8 pr-8 items-center">
       <li v-for="link in navRight" :key="link.path">
-        <router-link 
-          :to="link.path" 
+        <router-link :to="link.path"
           class="text-white hover:border-b-2 font-montserrat tracking-wide hover:border-yellowApp">
           {{ link.sectionName }}
         </router-link>
       </li>
-      
+
       <li>
-        <button class="flex flex-col items-center">
-          <router-link 
-            to="/cart" 
-            class="flex font-montserrat text-yellowApp items-center space-x-2">
-            <img 
-              src="/ion_ticket.png" 
-              alt="Ticket icon" 
-              class="h-6 w-6 mr-2"
-            >
-            <span>{{ cartStore.cartSize }}</span>
+        <button class="flex justify-center items-center text-xs">
+          <router-link to="/cart" class="flex flex-col items-center font-montserrat text-yellowApp text-center">
+            <img src="/ion_ticket.png" alt="Ticket icon" class="h-6 w-6 mb-1">
+            <span>{{ cartStore.cartSize }} tickets</span>
           </router-link>
         </button>
       </li>
+
     </ul>
   </nav>
 </template>
