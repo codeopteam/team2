c1141541818fb227eb5831506450753af9956b84
mava <mavafiza@gmail.com>
2024-12-12 16:30:11 +0100

    finishing spaces in navbar for responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,15 @@
 <template>
-  <header class="h-[65px] items-center bg-darkBlue text-white flex justify-end px-12
-">
-    <div class="flex items-center justify-between space-x-32 flex-grow ">
+  <header class="h-[65px] bg-darkBlue text-white flex items-center justify-between px-12">
+    <div class="flex items-center justify-between w-full space-x-32">
       <img class="h-[40px]" src="/LogoRectangular.png" alt="Logo de la pagina">
       <Navbar :navLeft="navLeft" :navRight="navRight"></Navbar>
     </div>
-      <Button></Button>
+
+    <Button></Button>
   </header>
 </template>
+
+
 <script>
 
 import Navbar from '../components/Navbar.vue'
@@ -18,28 +20,33 @@ export default {
     Navbar,
     Button,
   },
-  data(){
+  data() {
     return {
       navLeft: [
-        {path: "/",
-        sectionName: "Home" 
+        {
+          path: "/",
+          sectionName: "Home"
         },
-        {path: "/about",
-        sectionName: "About" 
+        {
+          path: "/about",
+          sectionName: "About"
         },
-        {path: "/contact",
-        sectionName: "Contact" 
+        {
+          path: "/contact",
+          sectionName: "Contact"
         },
       ],
       navRight: [
-        {path: "/register",
-        sectionName: "Register" 
+        {
+          path: "/register",
+          sectionName: "Register"
         },
-        {path: "/login",
-        sectionName: "Login" 
+        {
+          path: "/login",
+          sectionName: "Login"
         },
       ],
-      };
-    },
-  };
+    };
+  },
+};
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,36 @@
 <template>
-<nav class="p-4 flex flex-wrap justify-between items-center space-y-4 md:space-y-0">
+  <nav class="flex w-full justify-between">
     <ul class="flex space-x-8">
-      <li  v-for="link in navLeft" :key="link.path" >
-        <router-link :to="link.path" class="text-white-600 hover:border-b-2 font-montserrat tracking-wide hover:border-yellowApp">{{ link.sectionName }}</router-link>
+      <li v-for="link in navLeft" :key="link.path">
+        <router-link 
+          :to="link.path" 
+          class="text-white-600 hover:border-b-2 font-montserrat tracking-wide hover:border-yellowApp">
+          {{ link.sectionName }}
+        </router-link>
       </li>
-     
     </ul>
-     <ul class="flex space-x-8">
+
+    <ul class="flex space-x-8 pr-8">
       <li v-for="link in navRight" :key="link.path">
-        <router-link :to="link.path" class="text-white hover:border-b-2 font-montserrat tracking-wide hover:border-yellowApp">{{  link.sectionName }}</router-link>
+        <router-link 
+          :to="link.path" 
+          class="text-white hover:border-b-2 font-montserrat tracking-wide hover:border-yellowApp">
+          {{ link.sectionName }}
+        </router-link>
       </li>
       
-      <!-- <li v-if="cartStore.showTickets"> -->
       <li>
-        <button>
-          <router-link to="/cart" class="flex font-montserrat text-yellowApp "> <img src="/ion_ticket.png" alt="Ticket icon" class="h-6 w-6 mr-2">{{ cartStore.cartSize }}</router-link>
+        <button class="flex flex-col items-center">
+          <router-link 
+            to="/cart" 
+            class="flex font-montserrat text-yellowApp items-center space-x-2">
+            <img 
+              src="/ion_ticket.png" 
+              alt="Ticket icon" 
+              class="h-6 w-6 mr-2"
+            >
+            <span>{{ cartStore.cartSize }}</span>
+          </router-link>
         </button>
       </li>
     </ul>
@@ -22,28 +38,29 @@
 </template>
 
 
+
 <script>
-import  { useCartStore } from "../stores/cartStore"
+import { useCartStore } from "../stores/cartStore"
 import { mapStores } from "pinia";
 
 export default {
   name: "NavBar",
-  props: 
-     ["navLeft", "navRight"],
-     data(){
-      return {
+  props:
+    ["navLeft", "navRight"],
+  data() {
+    return {
 
-      };
-     },
-     
-    computed: {
-      ...mapStores(useCartStore)
-    },
-    // mounted() {
-    //   this.cartStore.getItemsFromFirebase()
+    };
+  },
 
-    // }
+  computed: {
+    ...mapStores(useCartStore)
+  },
+  // mounted() {
+  //   this.cartStore.getItemsFromFirebase()
+
+  // }
 
 };
 
-</script> 
+</script>
