2639bde589ebbc195f7d087c8a16a3031a9853b4
Yalina Gómez <yalinita1985@gmail.com>
2024-12-13 12:09:10 +0100

    component Map.vue created

---
+++ src/components/Map.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Mapboxgl</h1>
+</template>
+
+<script>
+export default {
+    name: "Map",
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted: {
+
+    },
+}
+</script>
+
+<style></style>
