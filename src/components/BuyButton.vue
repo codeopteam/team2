4e855d4bd4e6603b9697c420ea5439c170b6f242
AlejandroMurr <elkinmoore@gmail.com>
2024-12-14 13:00:58 +0100

    component button Buy Ticket completed

--- src/components/BuyButton.vue
+++ src/components/BuyButton.vue
@@ -1,7 +1,7 @@
 <template>
 
     <button class="flex items-center bg-yellowApp text-darkBlue font-montserrat text-2xl rounded-md px-6 py-2 gap-2">
-        <img src="/public/ticket.png" alt="Ticket Icon" class="w-6 h-6"> Buy Tickets</button>
+        <img src="/public/ticketBlue.png" alt="Ticket Icon" class="w-6 h-6"> Buy Tickets</button>
     
     
  </template>
