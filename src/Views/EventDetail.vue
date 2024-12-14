d9d8fdfd2813c5a2b8c60a25804eb268bd136316
Yalina Gómez <yalinita1985@gmail.com>
2024-12-14 16:12:43 +0100

    eventDetail with link to cart

--- src/Views/EventDetail.vue
+++ src/Views/EventDetail.vue
@@ -6,82 +6,95 @@
         alt="Event Image" />
     </div>
 
-    <div class="flex items-center justify-center w-full py-12">
-      <div class="w-11/12 border border-red-950 h-96 flex justify-between">
+    <div class="flex items-center justify-center w-full">
+      <div class="w-11/12 flex justify-between">
         <div>
-        <h3 class="text-2xl font-bold">{{ eventDetailresp.name }}</h3>
-        <h4 class="font-semibold mt-2 text-xl">Date and Time</h4>
-        <div class="flex gap-2 mt-2">
-          <img src="../../public/calendarIcon.svg" class="w-5" alt="Calendar Icon"/>
-          <p class="font-semibold mt-2 pb-2 ms-1">{{ eventDetailresp.dates.start.localDate }}</p>
-         </div>
-        <div class="flex gap-2 mt-2">
-          <img src="../../public/watchIcon.svg" class="w-6" alt="Watch Icon"/>
-        <p class="font-semibold">{{ new Date(`${eventDetailresp.dates.start.localDate}T${eventDetailresp.dates.start.localTime}`).toLocaleTimeString('en-EN', { hour: '2-digit', minute: '2-digit', hour12: false }) }}</p>
-        </div> 
+          <h3 class="text-2xl font-bold">{{ eventDetailresp.name }}</h3>
+          <h4 class="font-semibold mt-2 text-xl">Date and Time</h4>
+          <div class="flex gap-2 mt-2">
+            <img src="../../public/calendarIcon.svg" class="w-5" alt="Calendar Icon" />
+            <p class="font-semibold mt-2 pb-2 ms-1">{{ eventDetailresp.dates.start.localDate }}</p>
+          </div>
+          <div class="flex gap-2 mt-2">
+            <img src="../../public/watchIcon.svg" class="w-6" alt="Watch Icon" />
+            <p class="font-semibold">{{ new
+              Date(`${eventDetailresp.dates.start.localDate}T${eventDetailresp.dates.start.localTime}`).toLocaleTimeString('en-EN',
+                { hour: '2-digit', minute: '2-digit', hour12: false }) }}</p>
+          </div>
 
-       
-    </div>
 
-    <div>
-   <div class="flex justify-end self-start bg-red-200 mb-8">
-        <button class="flex items-end" @click="interested=!interested">            
-          <img v-if="interested" src="../../public/Interested Button2.png" alt="Start Icon" class="w-12"> 
-          <img v-else src="../../public/Interested Button.png" alt="Start Icon" class="w-12">         
-        <button>
-
-        </button>
-         <img src="../../public/Share button.png" alt="Share Icon" class="w-12 mx-2"/>
-        </button>
-
-        
-    </div>  
-
-     <!-- section buy ticket -->
-     <div class="border border-red-950 w-60 flex bg-blue-200">
-        <div>  
-          <h6 class="text-xl font-semibold my-2">Ticket Information</h6>   
-          <h4>Standard minimum price: {{ eventDetailresp.priceRanges[0].min }} €</h4>          
-          <h4>Standard including fees: {{ eventDetailresp.priceRanges[1].min }} €</h4>         
         </div>
-        
-      </div>
-        <!-- section buy ticket -->
-      </div>
 
+        <div>
+          <div class="flex justify-end self-start mb-8">
+            <button class="flex items-end" @click="interested = !interested">
+              <img v-if="interested" src="../../public/Interested Button2.png" alt="Start Icon" class="w-12">
+              <img v-else src="../../public/Interested Button.png" alt="Start Icon" class="w-12">
+              <button>
+
+              </button>
+              <img src="../../public/Share button.png" alt="Share Icon" class="w-12 mx-2" />
+            </button>
+
+
+          </div>
+
+          <!-- section buy ticket -->
+          <div class="w-60 flex">
+            <div>
+              <router-link to="/cart">
+                <BuyTicketButton class="mb-7 ms-4"></BuyTicketButton>
+              </router-link>
+              <h6 class="text-xl font-semibold my-3 ms-6">Ticket Information</h6>
+              <div class="flex ms-4">
+                <img src="../../public/ion_ticketBlue.png" alt="Ticket Icon" class="w-5">
+                <h4 class="ms-2">Standard min price: {{ eventDetailresp.priceRanges[0].min }} €</h4>
+                <!-- <h4>Standard including fees: {{ eventDetailresp.priceRanges[1].min }} €</h4>          -->
+              </div>
+            </div>
+          </div>
+          <!-- section buy ticket -->
+        </div>
       </div>
-      
     </div>
-   
 
 
-    <div class="flex items-center justify-center w-full py-12">
-      <div class="w-11/12 border border-red-950 h-96">
-        <div src="" frameborder="0" class="w-5/12 border border-orange-600 h-80">
-          <h6 class="text-xl font-semibold mb-2">Location</h6>
-          <h3>{{ eventDetailresp._embedded.venues[0].address?.line1 }}</h3>
-          <h3>{{ eventDetailresp._embedded.venues[0].city?.name }}</h3>
+    <div class="flex items-center justify-center w-full">
+      <div class="w-11/12 h-96">
+        <div src="" frameborder="0" class="w-5/12 h-80">
+          <h6 class="text-xl font-semibold mb-2 ms-2">Location</h6>
+          <div class="flex">
+            <img src="../../public/locationIcon.svg" alt="Location Icon" class="w-5 me-2">
+            <div>
+              <h3>{{ eventDetailresp._embedded.venues[0].address?.line1 }}</h3>
+              <div class="flex">
+                <h3>{{ eventDetailresp._embedded.venues[0]?.postalCode }}</h3>
+                <h3 class="ms-2">{{ eventDetailresp._embedded.venues[0].city?.name }}</h3>
+              </div>
+            </div>
+          </div>
           <Map :longitude="longitude" :latitude="latitude" class="mt-2"></Map>
-          
-    
+
         </div>
 
       </div>
-    </div> 
+    </div>
 
+
+    <div class="flex items-center justify-center w-full py-8">
+      <div class="w-11/12">
+        <h6 class="text-xl font-semibold mb-4 ms-6">Hosted By</h6>
+        <img src="../../public/ImageNameButtons.png" alt="City Youth Movement Logo" class="w-60">
+      </div>
+    </div>
 
     <!-- <div class="flex items-center justify-center w-full py-12">
       <div class="w-11/12 border border-red-950 h-96">
-        <p>Hosted By</p>
+
+        <h4>More Event Details:</h4>
+       
       </div>
     </div> -->
-
-    <div class="flex items-center justify-center w-full py-12">
-      <div class="w-11/12 border border-red-950 h-96">
-    
-        <h4>Event Description: {{ eventDetailresp.priceRanges[1].min }} €</h4>  
-      </div>
-    </div>
 
     <!-- <div class="flex items-center justify-center w-full py-12">
       <div class="w-11/12 border border-red-950 h-96">
@@ -109,6 +122,8 @@
 
 import axios from "axios";
 import Map from "../components/Map.vue";
+import BuyTicketButton from "../components/BuyButton.vue";
+
 export default {
   name: "EventDetail",
 
@@ -118,9 +133,9 @@ export default {
       loading: false,
       error: "",
       interested: false,
-    } 
+    }
   },
- 
+
   methods: {
     performSearch() {
 
@@ -130,7 +145,7 @@ export default {
       const apiKey = import.meta.env.VITE_API_KEY;
 
       let url = `https://app.ticketmaster.com/discovery/v2/events/${this.$route.params.id}.json?apikey=${apiKey}`;
-      
+
       axios(url)
         .then((response) => {
           console.log(response)
@@ -153,6 +168,7 @@ export default {
   },
   components: {
     Map,
+    BuyTicketButton,
   },
   mounted() {
     this.performSearch()
