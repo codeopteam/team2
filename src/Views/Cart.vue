5ec755bcf850059d90e55c5b23382b6a5042fbbf
mava <mavafiza@gmail.com>
2024-12-15 23:26:26 +0100

    button Pay Now styles

--- src/Views/Cart.vue
+++ src/Views/Cart.vue
@@ -2,28 +2,30 @@
 
   <div class="flex flex-col items-center justify-center gap-7">
 
+
+
     <!-- MOdal de Cart con logica init-->
-    <div v-if="!showModal" class="bg-gray-50 w-1/2 overflow-y-auto pb-4 min-h-[60vh] flex flex-col mt-20 mb-20">
+    <div v-if="!showModal" class="bg-gray-50 w-1/2 overflow-y-auto pb-4 min-h-[60vh] flex flex-col mt-10 mb-10">
       <!-- Título -->
-      <div class="bg-darkBlue shadow-lg rounded-lg flex justify-between items-center p-6">
-        <h2 class="text-2xl font-semibold font-montserrat text-yellowApp">Select Tickets</h2>
+      <div class="bg-darkBlue w-full shadow-lg rounded-md flex justify-between items-center p-6">
+        <h2 class="text-xl font-semibold font-montserrat text-yellowApp">Select Tickets</h2>
       </div>
 
       <!-- Encabezados Ticket Types y Quantity -->
       <div class="mt-14 grid grid-cols-2 gap-4 w-auto">
-        <p class="text-md font-montserrat ml-8">Ticket Types</p>
-        <p class="text-md font-montserrat mr-10 text-right">Quantity</p>
+        <p class="text-sm font-montserrat ml-8">Ticket Types</p>
+        <p class="text-sm font-montserrat mr-10 text-right">Quantity</p>
       </div>
 
       <!-- Lista de Tickets -->
       <ul v-for="(item, index) in cartStore.items"
         class="bg-white mt-4 border-b flex flex-col shadow-lg rounded-lg items-end w-auto">
         <li
-          class="flex h-32 border-l-8 border-yellowApp text-xl font-montserrat text-left pl-8 justify-between items-center w-full">
+          class="flex h-20 border-l-8 border-yellowApp text-md font-montserrat text-left pl-8 justify-between items-center w-full">
           <span class="w-2/3">{{ item.name }}</span>
           <div class="ml-auto pr-8">
-            <button class="border border-orange-600 rounded-md px-1 bg-orange-600" @click="cartStore.deleteItem(index)">
-              x
+            <button class="shadow-lg rounded-xl w-6 px-1 " @click="cartStore.deleteItem(index)">
+              <img src="/icon _trash.png" alt="">
             </button>
           </div>
         </li>
@@ -34,7 +36,7 @@
         <div class="flex justify-between items-center text-center mb-4">
           <div class="text-lg font-semibold w-1/2">
             <span class="mr-2">Qty:</span>
-            <span class="text-bold">{{  cartStore.items.length}}</span>
+            <span class="text-bold">{{ cartStore.items.length }}</span>
           </div>
           <div class="text-lg font-semibold w-1/2">
             <span class="mr-2">Total:</span>
@@ -43,56 +45,52 @@
         </div>
 
         <div class="flex justify-center">
-          <button class="bg-darkBlue text-yellowApp px-60 py-6 rounded-md text-xl font-montserrat mt-7" @click="showModal = true">
+          <button
+            class="bg-darkBlue text-yellowApp w-11/12 rounded-md text-xl font-montserrat flex items-center justify-center h-12 flex-row"
+            @click="showModal = true">
             Proceed >
           </button>
         </div>
       </div>
 
-
     </div>
     <!-- MOdal de Cart con logica end-->
 
-
     <!-- inicio modal Pay Now  -->
 
-    <div v-if="showModal" class="bg-gray-50 w-1/2 min-h-[60vh] mt-20 mb-40">
-      <div class=" bg-yellowApp rounded-lg shadow-lg p-6">
-        <div class="flex justify-between items-center p-1">
-          <h2 class="text-2xl font-semibold font-montserrat">
-            < Order Sumary</h2>
-        </div>
+    <div v-if="showModal" class="flex flex-col pb-4  justify-center items-center overflow-y-auto bg-gray-50 w-1/2 min-h-[60vh] mt-10 mb-10  gap-5">
+
+      <div class="w-full bg-yellowApp flex justify-between items-center p-4 rounded-md shadow-lg">
+        <h2 class="text-xl font-semibold font-montserrat">
+          < Order Sumary </h2>
+
       </div>
 
-      <div class="bg-white mt-24 shadow-lg rounded-lg ml-40 mr-32">
-        <div class="border-t-8 border-yellowApp">
+      <div class="w-4/6 h-24 bg-white mt-4 shadow-lg rounded-md border-t-4 border-t-yellowApp border">
 
-          <div>
-            <p class="text-2xl font-montserrat text-center mt-8">Standard Ticket</p>
-            <div class="ml-5 mt-11">
-              <p class="text-2xl font-montserrat mt-2">EvenTop</p>
+        <div class="h-full w-full">
+          <p class="text-sm font-montserrat text-center font-semibold mt-5">Standard Ticket</p>
+          <div class="ml-5">
+            <p class="text-xs font-montserrat">EvenTop</p>
 
-              <div class="flex justify-between pb-12">
-                <p class="text-2xl font-montserrat">Your Ticket </p>
-                <p class="px-8 py-1 text-xl font-montserrat text-darkBlue bg-yellowApp">40€</p>
-              </div>
-
+            <div class="flex justify-between">
+              <p class="text-xs font-montserrat">Your Ticket </p>
+              <p class="px-8 text-xs font-montserrat text-darkBlue bg-yellowApp">40€</p>
             </div>
-          </div>
 
+          </div>
         </div>
 
       </div>
 
-      <div class="bg-white px-6 py-4 border-t shadow-xl mt-32">
+      <div class="bg-white px-6 py-4 border-t shadow-xl font-montserrat mt-auto w-auto h-48 flex flex-col justify-between">
 
-        <div class=" flex justify-between font-montserrat text-2xl mb-8 ml-40 mr-40 mt-8">
+        <div class=" flex justify-evenly text-center font-montserrat text-sm">
 
           <div>
             <p> Sub Total:</p>
             <p> Tax:</p>
           </div>
-
           <div>
             <p> 40€</p>
             <p> 10€</p>
@@ -100,24 +98,28 @@
 
         </div>
 
+        <!-- <div class="bg-slate-400 "> </div> -->
 
-
-        <div class="bg-slate-400 m-1 h-0.5 mb-10"> </div>
-
-        <div class=" flex justify-center gap-48 text-3xl font-montserrat">
-          <p>Order Total:</p>
-          <p>50€</p>
-
+        <div class="flex justify-center text-sm font-montserrat">
+          <p>Order Total: </p>
+          <p> 50€</p>
         </div>
 
-
-        <div class="flex justify-center">
-          <button class="px-60 py-6 rounded-md text-2xl font-montserrat mt-7 bg-yellowApp text-darkBlue">
-            < Pay Now>
+        <div class="flex justify-center w-full">
+          <button
+            class="px-60 rounded-md text-md text-center font-montserrat bg-yellowApp w-11/12 text-darkBlue flex items-center justify-center h-12 flex-row text-nowrap gap-6 ">
+            <img src="/bag.png" alt="">Pay Now
           </button>
+          
         </div>
       </div>
+
     </div>
+
+
+
+    <!-- final modal Pay Now  -->
+
 
 
 
