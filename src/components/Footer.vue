7e1d510f8e5642ca03c4f3b94979738968147f27
mava <mavafiza@gmail.com>
2024-12-16 00:38:12 +0100

    aligning footer containers

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,110 +1,87 @@
 <template>
-    <footer class="bg-darkBlue text-white py-10">
+  <footer class="bg-darkBlue text-white py-10">
 
-      <div class="container mx-auto px-10 lg:px-20 grid grid-cols-5 md:gap-8">
+    <div class="container mx-auto px-10 lg:px-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
 
-        <div class="flex flex-col">
-
-        <h3 class="text-white font-montserrat text-xl mb-4"> Company Info</h3>
-
+      <!-- Sección 1 -->
+      <div class="flex flex-col">
+        <h3 class="text-white font-montserrat text-xl mb-4">Company Info</h3>
         <ul class="space-y-2 text-xs font-light">
-
-          <li> <a href="#" class="hover:text-white">About Us</a></li>
-          <li> <a href="#" class="hover:text-white">Contact Us</a> </li>
-          <li> <a href="#" class="hover:text-white">Careers</a> </li>
-          <li> <a href="#" class="hover:text-white">FAQs</a> </li>
-          <li> <a href="#" class="hover:text-white">Terms of Service</a> </li>
-          <li> <a href="#" class="hover:text-white">Privacy Policy</a> </li>
-
+          <li><a href="#" class="hover:text-white">About Us</a></li>
+          <li><a href="#" class="hover:text-white">Contact Us</a></li>
+          <li><a href="#" class="hover:text-white">Careers</a></li>
+          <li><a href="#" class="hover:text-white">FAQs</a></li>
+          <li><a href="#" class="hover:text-white">Terms of Service</a></li>
+          <li><a href="#" class="hover:text-white">Privacy Policy</a></li>
         </ul>
-
       </div>
 
+      <!-- Sección 2 -->
       <div class="flex flex-col">
-
-        <h3 class="text-white font-montserrat text-xl mb-4"> Help</h3>
-
+        <h3 class="text-white font-montserrat text-xl mb-4">Help</h3>
         <ul class="space-y-2 text-xs font-light">
-
-          <li> <a href="#" class="hover:text-white">Account Support</a></li>
-          <li> <a href="#" class="hover:text-white">Listing Events</a> </li>
-          <li> <a href="#" class="hover:text-white">event Ticketing</a> </li>
-          <li> <a href="#" class="hover:text-white">Ticket Purchase Terms & Conditions</a> </li>
-        
-
+          <li><a href="#" class="hover:text-white">Account Support</a></li>
+          <li><a href="#" class="hover:text-white">Listing Events</a></li>
+          <li><a href="#" class="hover:text-white">Event Ticketing</a></li>
+          <li><a href="#" class="hover:text-white">Ticket Purchase Terms & Conditions</a></li>
         </ul>
-
       </div>
 
+      <!-- Sección 3 -->
       <div class="flex flex-col">
-
-        <h3 class="text-white font-montserrat text-xl mb-4"> Categories</h3>
-
+        <h3 class="text-white font-montserrat text-xl mb-4">Categories</h3>
         <ul class="space-y-2 text-xs font-light">
-
-          <li> <a href="#" class="hover:text-white">Concert & Gigs</a></li>
-          <li> <a href="#" class="hover:text-white">Festival & Lifestyle</a> </li>
-          <li> <a href="#" class="hover:text-white">Business & Networking</a> </li>
-          <li> <a href="#" class="hover:text-white">Food & Drinks</a> </li>
-          <li> <a href="#" class="hover:text-white">Performing Arts</a> </li>
-          <li> <a href="#" class="hover:text-white">Sports & Outdoors</a> </li>
-          <li> <a href="#" class="hover:text-white">Exhibitions</a> </li>
-          <li> <a href="#" class="hover:text-white">Workshops, Conferences & Classes</a> </li>
-
-
+          <li><a href="#" class="hover:text-white">Concert & Gigs</a></li>
+          <li><a href="#" class="hover:text-white">Festival & Lifestyle</a></li>
+          <li><a href="#" class="hover:text-white">Business & Networking</a></li>
+          <li><a href="#" class="hover:text-white">Food & Drinks</a></li>
+          <li><a href="#" class="hover:text-white">Performing Arts</a></li>
+          <li><a href="#" class="hover:text-white">Sports & Outdoors</a></li>
+          <li><a href="#" class="hover:text-white">Exhibitions</a></li>
+          <li><a href="#" class="hover:text-white">Workshops, Conferences & Classes</a></li>
         </ul>
-
       </div>
 
+      <!-- Sección 4 -->
       <div class="flex flex-col">
-        <h3 class="text-white font-montserrat text-xl mb-4 ms-4 md:ms-0"> Follow Us</h3>
-
+        <h3 class="text-white font-montserrat text-xl mb-4">Follow Us</h3>
         <ul class="space-y-2 text-xs font-light">
-
-        <li> <a href="#" class="hover:text-white">Facebook</a></li>
-          <li> <a href="#" class="hover:text-white">Instagram</a> </li>
-          <li> <a href="#" class="hover:text-white">Twitter</a> </li>
-          <li> <a href="#" class="hover:text-white"> Youtube</a> </li>
-
+          <li><a href="#" class="hover:text-white">Facebook</a></li>
+          <li><a href="#" class="hover:text-white">Instagram</a></li>
+          <li><a href="#" class="hover:text-white">Twitter</a></li>
+          <li><a href="#" class="hover:text-white">YouTube</a></li>
         </ul>
-
-
       </div>
 
+      <!-- Sección 5 -->
       <div class="flex flex-col">
-  <h3 class="text-white font-montserrat text-xl mb-4">Download the App</h3>
+        <h3 class="text-white font-montserrat text-xl mb-4">Download the App</h3>
+        <div class="flex items-center justify-center border border-gray-500 rounded p-2 mb-4">
+          <img src="/logoGooglePlay.png" alt="Google Play" class="w-12 h-auto ml-2 mr-4">
+          <div class="text-left">
+            <p class="text-white text-sm">Get it on</p>
+            <p class="text-white text-lg font-montserrat">Google Play</p>
+          </div>
+        </div>
 
-  <div class="flex flex-col lg:flex-row items-center border border-gray-500 rounded p-2 mb-4">
-    <img src="/logoGooglePlay.png" alt="Google Play" class="w-12 h-auto ml-2 mr-4">
-    <div class="text-center mt-1 lg:mt-0">
-      <p class="text-white text-sm">Get it on</p>
-      <p class="text-white text-lg font-monserrat">Google Play</p>
+        <div class="flex items-center justify-center border border-gray-500 rounded p-2 mb-4">
+          <img src="/logoAppel.png" alt="App Store" class="w-6 h-auto ml-2 mr-4">
+          <div class="text-left mt-2">
+            <p class="text-white text-sm">Download on the</p>
+            <p class="text-white text-lg font-montserrat">App Store</p>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
 
-  <div class="flex flex-col lg:flex-row items-center border border-gray-500 rounded p-2">
-    <img src="/logoAppel.png" alt="App Store" class="w-6 h-auto ml-4 mr-8">
-    <div class="text-center mt-1 lg:mt-0">
-      <p class="text-white text-sm">Download on the</p>
-      <p class="text-white text-lg font-monserrat">App Store</p>
+    <!-- Footer inferior -->
+    <div class="bg-darkBlue py-4 border-t border-gray-600 mt-8">
+      <div class="container mx-auto text-white text-xs">
+        <p class="text-center">© 2024 EvenTop. All rights reserved.</p>
+      </div>
     </div>
-  </div>
-</div>
 
-</div>
-
-<div class="bg-darkBlue py-4 border-t border-gray-600 mt-8">
-
-  <div class="container mx-auto text-white text-xs">
-    <p class="mb-4 md:mb-0 text-center"> © 2024 EvenTop. All rights reserved.</p>
-
-  </div>
+  </footer>
 
 
-
-         </div>
-         
-    </footer>
-
-  </template>
-  
+</template>
