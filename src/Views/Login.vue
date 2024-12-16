260bac305d8c1b48e418e4b96319fa61d3d13a0c
mava <mavafiza@gmail.com>
2024-12-16 03:53:37 +0100

    routing all links from footer to /login

--- src/Views/Login.vue
+++ src/Views/Login.vue
@@ -1,7 +1,30 @@
 <template>
-    <div>
-      <h1 class="text-2xl font-bold">Iniciar Sesión</h1>
-      <p>Introduce tus credenciales para iniciar sesión.</p>
-    </div>
-  </template>
-  
+  <div class=" not-found justify-start items-start pl-2 pt-8">
+      <button @click="goHome" class="flex flex-col w-24 justify-center items-center">
+          <img src="/house.gif" alt="" class="flex rounded w-12 justify-center items-center">
+          <p class="text-xs">Go Back Home</p>
+      </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  methods: {
+      goHome() {
+          this.$router.push({ name: "Home" });
+      },
+  },
+};
+</script>
+
+<style scoped>
+.not-found {
+  height: 100vh;
+  background-image: url('/balls.gif');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  color: white;
+}
+</style>
--- src/Views/Register.vue
+++ src/Views/Register.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="flex flex-row">
+
+    
     <!-- Primer div con fondo oscuro -->
     <div class="w-2/5 h-screen bg-darkBlue flex justify-center items-center">
       <p class="text-white text-5xl w-5/6 font-montserrat">Discover tailored <br> events. <br>
         Sign up for personalized <br> recommendations <br> today!</p>
     </div>
     <!-- Segundo div con fondo blanco -->
-    <div class="w-2/5 h-screen bg-darkBlue">
+    <div class="w-3/5 h-screen bg-darkBlue">
       <div class="flex flex-col w-full rounded-l-[50px] rounded h-screen bg-white justify-center items-left">
         <p class="text-black">Creatte Account</p>
         <div class="border">
@@ -15,14 +17,18 @@
           <div>linea</div>
         </div>
         <form action="">
-          <input type="text">
-          <input type="text">
-          <input type="text">
-          <button>Create Account</button>
-          <a href="">Already have an account? Log In <span>Log In</span></a>
+          <div class="flex flex-col">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+            <button>Create Account</button>
+            <a href="">Already have an account? Log In <span>Log In</span></a>
+          </div>
         </form>
       </div>
     </div>
+
+
   </div>
 </template>
 
