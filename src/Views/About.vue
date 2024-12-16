d390079bac271d9004cd8d4b594fe08c4a492a68
mava <mavafiza@gmail.com>
2024-12-16 02:19:34 +0100

    creating json of About for dinamics cards

--- src/Views/About.vue
+++ src/Views/About.vue
@@ -1,48 +1,68 @@
 <template>
-  <div class="flex justify-center items-center h-96 gap-9 ">
-
-
-    
-    <div class="w-1/6 justify-center items-center text-center text-xs">
-      <img class="rounded-full shadow-lg " src="/zoraida.jpeg" alt="">
-      <p class="text-sm">Zora</p>
-      <p>Meditativa</p>
+<div class="flex justify-center items-center h-96 gap-9 flex-col lg:flex-row">
+    <div v-for="(person, index) in people" :key="index" class="w-1/6 justify-center items-center text-center text-xs">
+      <img class="rounded-full shadow-lg" :src="person.image" alt="">
+      <p class="text-sm">{{ person.name }}</p>
+      <p>{{ person.description }}</p>
       <div class="flex justify-around items-center">
-        <a href="https://www.linkedin.com/in/zoraida-moreno/"><img class="rounded w-6" src="/linkedIn-logo2.gif"
-          alt=""></a>
-          <a href="https://github.com/ZoraidaMorenoCadenas"><img class="rounded w-6" src="/github-logo.gif" alt=""></a>
-        </div>
+        <a :href="person.links.linkedin" target="_blank">
+          <img class="rounded w-6" src="/linkedIn-logo2.gif" alt="">
+        </a>
+        <a :href="person.links.github" target="_blank">
+          <img class="rounded w-6" src="/github-logo.gif" alt="">
+        </a>
       </div>
-      <div class="w-1/6 justify-center items-center text-center text-xs">
-        <img class="rounded-full shadow-lg " src="/yalina.png" alt="">
-        <p class="text-sm">Yali</p>
-        <p>La Veloz</p>
-        <div class="flex justify-around items-center">
-          <a href="https://www.linkedin.com/in/yalinag%C3%B3mez-frontend-developer/"><img class="rounded w-6" src="/linkedIn-logo2.gif"
-            alt=""></a>
-            <a href="https://github.com/yali1986"><img class="rounded w-6" src="/github-logo.gif" alt=""></a>
-          </div>
-        </div>
-        <div class="w-1/6 justify-center items-center text-center text-xs">
-          <img class="rounded-full shadow-lg " src="/mavita.png" alt="">
-          <p class="text-sm">Ale</p>
-          <p>El Paciente</p>
-          <div class="flex justify-around items-center">
-            <a href="https://www.linkedin.com/in/mavafigalloz/"><img class="rounded w-6" src="/linkedIn-logo2.gif"
-              alt=""></a>
-              <a href="https://github.com/AlejandroMurr"><img class="rounded w-6" src="/github-logo.gif" alt=""></a>
-            </div>
-          </div>
-          <div class="w-1/6 justify-center items-center text-center text-xs">
-            <img class="rounded-full shadow-lg " src="/mavita.jpg" alt="">
-            <p class="text-sm">Mava</p>
-            <p>La Persistente</p>
-            <div class="flex justify-around items-center">
-              <a href="https://www.linkedin.com/in/mavafigalloz/"><img class="rounded w-6" src="/linkedIn-logo2.gif"
-                  alt=""></a>
-              <a href="https://github.com/mavafiza"><img class="rounded w-6" src="/github-logo.gif" alt=""></a>
-            </div>
-          </div>
-          
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      people: [
+        {
+          name: "Zora",
+          description: "Meditativa",
+          image: "/zoraida.jpeg",
+          links: {
+            linkedin: "https://www.linkedin.com/in/zoraida-moreno/",
+            github: "https://github.com/ZoraidaMorenoCadenas"
+          }
+        },
+        {
+          name: "Yali",
+          description: "La Veloz",
+          image: "/yalina.png",
+          links: {
+            linkedin: "https://www.linkedin.com/in/yalinag%C3%B3mez-frontend-developer/",
+            github: "https://github.com/yali1986"
+          }
+        },
+        {
+          name: "Ale",
+          description: "El Paciente",
+          image: "/mavita.png",
+          links: {
+            linkedin: "https://www.linkedin.com/in/mavafigalloz/",
+            github: "https://github.com/AlejandroMurr"
+          }
+        },
+        {
+          name: "Mava",
+          description: "La Persistente",
+          image: "/mavita.jpg",
+          links: {
+            linkedin: "https://www.linkedin.com/in/mavafigalloz/",
+            github: "https://github.com/mavafiza"
+          }
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* Puedes añadir estilos adicionales si lo necesitas */
+</style>
