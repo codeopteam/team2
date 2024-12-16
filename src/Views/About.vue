757be68b436f52d248f27c284b6c4a34a4b2ea3d
AlejandroMurr <elkinmoore@gmail.com>
2024-12-16 15:48:49 +0100

    change of background color  of the images

--- src/Views/About.vue
+++ src/Views/About.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center items-center h-auto my-10 gap-5 flex-col md:flex-row">
     <div v-for="(person, index) in people" :key="index" class="w-40 justify-center items-center text-center text-xs">
       <img class="rounded-full shadow-lg" :src="person.image" alt="">
-      <p class="text-sm">{{ person.name }}</p>
+      <p class="text-lg mt-1">{{ person.name }}</p>
       <p>{{ person.description }}</p>
       <div class="flex justify-around items-center">
         <a :href="person.links.linkedin" target="_blank">
@@ -24,7 +24,7 @@ export default {
         {
           name: "Zora",
           description: "La Meditativa",
-          image: "/zoraida.jpeg",
+          image: "/zoraida.png",
           links: {
             linkedin: "https://www.linkedin.com/in/zoraida-moreno/",
             github: "https://github.com/ZoraidaMorenoCadenas"
@@ -42,16 +42,16 @@ export default {
         {
           name: "Ale",
           description: "El Paciente",
-          image: "/gastly.gif",
+          image: "/alejo.png",
           links: {
-            linkedin: "https://www.linkedin.com/in/mavafigalloz/",
+            linkedin: "https://www.linkedin.com/in/alejandro-murr-4287a0202/",
             github: "https://github.com/AlejandroMurr"
           }
         },
         {
           name: "Mava",
           description: "La Persistente",
-          image: "/mavita.jpg",
+          image: "/mava.png",
           links: {
             linkedin: "https://www.linkedin.com/in/mavafigalloz/",
             github: "https://github.com/mavafiza"
