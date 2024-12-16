1304c2d5701bede630f4978589b1ba38a19de30a
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2024-12-16 20:49:35 +0100

    change button name to logout and responsive in header

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,7 +12,7 @@
     </ul>
 
     <!-- Right Links -->
-    <ul class="flex pr-8 items-center gap-3 lg:gap-5">
+    <ul class="flex items-center gap-3 lg:gap-5">
       <li v-for="link in navRight" :key="link.path">
         <router-link :to="link.path"
           class="text-white hover:border-b-2 font-montserrat tracking-wide hover:border-yellowApp hidden md:block ms-2"
@@ -34,12 +34,12 @@
         </button>
       </li>
 
-      <Button class="mx-4 md:mx-6"></Button>
+      <Button class="mx-4 md:mx-6 hidden md:block"></Button>
 
       <!-- Button hamburguer -->
       <div class="block md:hidden -me-12">
         <button @click="showLinks()" class="text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
           </svg>
         </button>
