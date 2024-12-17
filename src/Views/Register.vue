ecd76208a2534dc715a8484877348a75131fc949
mava <mavafiza@gmail.com>
2024-12-17 01:25:53 +0100

    separating buttons

--- src/Views/Register.vue
+++ src/Views/Register.vue
@@ -15,7 +15,7 @@
         <div class="mx-32">
 
           <h2 class="text-3xl font-bold text-center mb-6">Create Account</h2>
-          <div class="flex border">
+          <div class="flex gap-2">
 
             <button
               class="flex items-center justify-center w-1/2 py-2 border border-gray-300 rounded-lg text-gray-700 font-medium hover:bg-gray-100 transition">
