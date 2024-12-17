90912c6044ebd6815ec92396ad5d6c2fc300ea8b
Yalina Gómez <yalinita1985@gmail.com>
2024-12-17 22:58:52 +0100

    coments delete in map.vue

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -22,29 +22,22 @@ export default {
             type: Number,
             required: true,
         },
-    },
-    // methods: {
-
-    // },
+    },    
     mounted() {
-        if (this.latitude && this.longitude) {
-            console.log(this.latitude)
-            console.log(this.longitude)
-           this.map = L.map("map").setView([this.latitude, this.longitude], 13); // Coordenadas y zoom inicial (Madrid)
+        if (this.latitude && this.longitude) {            
+           this.map = L.map("map").setView([this.latitude, this.longitude], 13);
         // Capa de OpenStreetMap
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         }).addTo(this.map);
-
-        // Añade un marcador de ejemplo
+        
         L.marker([this.latitude, this.longitude])
             .addTo(this.map)
             .bindPopup("Here!")
             .openPopup();  
         } else {
             console.error("Coordenadas no válidas para inicializar el mapa.");
-        }
-       
+        }       
     },
 };
 </script>
