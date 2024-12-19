bddd48b74975801d3c5f1c2959c73d7d7deaa3c3
mava <mavafiza@gmail.com>
2024-12-20 00:42:16 +0100

    adding styles to hero mobile size

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,23 +1,20 @@
 <template>
-  <div :class="['bg-cover p-8 bg-[#2B293D] opacity-95']" :style="{ backgroundImage: `url(${imagePath})` }">
+  <div :class="['bg-cover p-8 bg-[#2B293D] opacity-95']" class="" :style="{ backgroundImage: `url(${imagePath})` }">
     <div class="flex justify-center">
-      <div>
-        <h2 class="text-4xl font-bold text-white mt-20 font-montserrat">{{ mainText }}</h2>
+      <div class="mt-4 sm:mt-10 md:mt-20">
+        <h2 class="text-4xl font-bold text-white font-montserrat">{{ mainText }}</h2>
         <h3 class="text-4xl font-bold text-white mt-2 font-montserrat">
           {{ subTextBefore }} <span class="text-yellowApp">{{ highlightedText }}</span> {{ subTextAfter }}
         </h3>
-        <div class="w-8/12 mx-auto">
-        <input
-          type="text"
-          v-model="inputCityUser"
-          class="mt-8 mb-8 rounded-md bg-[url('/Lupa.png')] bg-no-repeat bg-[left_14px_center] bg-[length:25px_25px] pl-14 w-full p-3 placeholder:text-2xl placeholder:text-[#636363]"
-          placeholder="City"
-          @keydown.enter="handleKeyDown"
-        />
-      </div>
+        <div class="w-full sm:w-8/12 mx-auto">
+          <input type="text" v-model="inputCityUser"
+            class="mt-8 mb-8 rounded-md bg-[url('/Lupa.png')] bg-no-repeat bg-[left_14px_center] bg-[length:25px_25px] pl-14 w-full p-3 placeholder:text-2xl placeholder:text-[#636363]"
+            placeholder="City" @keydown.enter="handleKeyDown" />
+        </div>
       </div>
     </div>
   </div>
+
 </template>
 
 <script>
@@ -48,7 +45,7 @@ export default {
       type: String,
       default: "",
     },
-   
+
   },
   data() {
     return {
